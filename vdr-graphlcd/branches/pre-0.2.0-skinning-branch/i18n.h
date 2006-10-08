80a4de995aa75b933b378bc09c76cb03efc9a91f
powarman <powarman@1ab4b1c3-03f5-0310-8ded-90bd03145d39>
2006-10-08 09:41:44 +0000

    - some reformatting

--- vdr-graphlcd/branches/pre-0.2.0-skinning-branch/i18n.h
+++ vdr-graphlcd/branches/pre-0.2.0-skinning-branch/i18n.h
@@ -1,33 +1,17 @@
-/**
- *  GraphLCD plugin for the Video Disk Recorder 
+/*
+ * GraphLCD plugin for the Video Disk Recorder
  *
- *  i18n.h  -  Internationalization
+ * i18n.h - Internationalization
  *
- *  (c) 2001-2004 Carsten Siebholz <c.siebholz AT t-online de>
- **/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program;                                              *
- *   if not, write to the Free Software Foundation, Inc.,                  *
- *   59 Temple Place, Suite 330, Boston, MA  02111-1307  USA               *
- *                                                                         *
- ***************************************************************************/
+ * This file is released under the GNU General Public License. Refer
+ * to the COPYING file distributed with this package.
+ *
+ * (c) 2001-2004 Carsten Siebholz <c.siebholz AT t-online.de>
+ * (c) 2004 Andreas Regel <andreas.regel AT powarman.de>
+ */
 
 #ifndef _GRAPHLCD_I18N_H_
 #define _GRAPHLCD_I18N_H_
-
 
 #include <vdr/i18n.h>
 
