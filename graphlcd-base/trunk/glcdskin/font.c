15e185b301069a5bc3810bc6eea959db96c00d33
powarman <powarman@1ab4b1c3-03f5-0310-8ded-90bd03145d39>
2007-01-06 15:26:33 +0000

    - cleaned up building of font path string - look in default font path when font file not in skin's font directory

--- graphlcd-base/trunk/glcdskin/font.c
+++ graphlcd-base/trunk/glcdskin/font.c
@@ -1,5 +1,7 @@
 #include <syslog.h>
 
+#include <fstream>
+
 #include "font.h"
 #include "skin.h"
 #include "function.h"
@@ -17,24 +19,12 @@ cSkinFont::cSkinFont(cSkin * Parent)
 
 bool cSkinFont::ParseUrl(const std::string & url)
 {
+    std::string::size_type count = std::string::npos;
+
     if (url.find("fnt:") == 0)
     {
         mType = ftFNT;
-        if (url[4] == '/' || url.find("./") == 4 || url.find("../") == 4)
-            mFile = url.substr(4);
-        else
-        {
-            mFile = mSkin->Config().SkinPath();
-            if (mFile.length() > 0)
-            {
-                if (mFile[mFile.length() - 1] != '/')
-                    mFile += '/';
-            }
-            mFile += "fonts/";
-            mFile += url.substr(4);
-        }
         mSize = 0;
-        return mFont.LoadFNT(mFile);
     }
     else if (url.find("ft2:") == 0)
     {
@@ -47,25 +37,56 @@ bool cSkinFont::ParseUrl(const std::string & url)
         }
         std::string tmp = url.substr(pos + 1);
         mSize = atoi(tmp.c_str());
-        if (url[4] == '/' || url.find("./") == 4 || url.find("../") == 4)
-            mFile = url.substr(4, pos - 4);
-        else
-        {
-            mFile = mSkin->Config().SkinPath();
-            if (mFile.length() > 0)
-            {
-                if (mFile[mFile.length() - 1] != '/')
-                    mFile += '/';
-            }
-            mFile += "fonts/";
-            mFile += url.substr(4, pos - 4);
-        }
-        return mFont.LoadFT2(mFile, mSkin->Config().CharSet(), mSize);
+        count = pos - 4;
     }
     else
     {
         syslog(LOG_ERR, "cSkinFont::ParseUrl(): Unknown font type in %s\n", url.c_str());
         return false;
+    }
+
+    if (url[4] == '/' || url.find("./") == 4 || url.find("../") == 4)
+        mFile = url.substr(4, count);
+    else
+    {
+        // first try skin's font dir
+        mFile = mSkin->Config().SkinPath();
+        if (mFile.length() > 0)
+        {
+            if (mFile[mFile.length() - 1] != '/')
+                mFile += '/';
+        }
+        mFile += "fonts/";
+        mFile += url.substr(4, count);
+#if (__GNUC__ < 3)
+        std::ifstream f(mFile.c_str(), std::ios::in | std::ios::binary);
+#else
+        std::ifstream f(mFile.c_str(), std::ios_base::in | std::ios_base::binary);
+#endif
+        if (f.is_open())
+        {
+            f.close();
+        }
+        else
+        {
+            // then try generic font dir
+            mFile = mSkin->Config().FontPath();
+            if (mFile.length() > 0)
+            {
+                if (mFile[mFile.length() - 1] != '/')
+                    mFile += '/';
+            }
+            mFile += url.substr(4, count);
+        }
+    }
+
+    if (mType == ftFNT)
+    {
+        return mFont.LoadFNT(mFile);
+    }
+    else
+    {
+        return mFont.LoadFT2(mFile, mSkin->Config().CharSet(), mSize);
     }
 }
 
